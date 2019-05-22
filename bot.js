4d3a9aa50c24cdf6848dae3bcedbe2764bede774
Artur Kasymov <art.kasymov@gmail.com>
2019-05-22 23:15:14 +0200

    init bot commit

---
+++ bot.js
@@ -0,0 +1,188 @@
+'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
+
+const GREETING = 'GREETING';
+
+//temp
+const RESEND = 'RESEND';
+
+const
+  request = require('request'),
+  express = require('express'),
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json());
+
+  //here should be init db
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+app.post('/webhook', (req, res) => {
+
+  // Return a '200 OK' response to all events
+  res.status(200).send('EVENT_RECEIVED');
+
+  const body = req.body;
+
+  if (body.object === 'page') {
+      if (body.entry && body.entry.length <= 0){
+        return;
+      }
+      body.entry.forEach((pageEntry) => {
+        // Iterate over each messaging event and handle accordingly
+        pageEntry.messaging.forEach((messagingEvent) => {
+          console.log({messagingEvent});
+          if (messagingEvent.postback) {
+            handlePostback(messagingEvent.sender.id, messagingEvent.postback);
+          } else if (messagingEvent.message) {
+            if (messagingEvent.message.quick_reply){
+              handlePostback(messagingEvent.sender.id, messagingEvent.message.quick_reply);
+            } else{
+              handleMessage(messagingEvent.sender.id, messagingEvent.message);
+            }
+          } else {
+            console.log(
+              'Webhook received unknown messagingEvent: ',
+              messagingEvent
+            );
+          }
+        });
+      });
+    }
+});
+
+app.get('/webhook', (req, res) => {
+
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+
+function handleMessage(sender_psid, message) {
+  	// check if it is a location message
+  	console.log('handleMEssage message:', JSON.stringify(message));
+
+  
+  	if(Math.random() >= 0.5){
+   		handlePostback(sender_psid, {payload: GREETING});
+   		return;
+	}else {
+		//temp
+		const resendPayload = {
+			"text": message.text.substring(0, 200)
+		}
+		callSendAPI(sender_psid, resendPayload);
+		return;
+	}
+}
+
+function handleGreetingPostback(sender_psid){
+  request({
+    url: `${FACEBOOK_GRAPH_API_BASE_URL}${sender_psid}`,
+    qs: {
+      access_token: process.env.PAGE_ACCESS_TOKEN,
+      fields: "first_name"
+    },
+    method: "GET"
+  }, function(error, response, body) {
+    var greeting = "";
+    if (error) {
+      console.log("Error getting user's name: " +  error);
+    } else {
+      var bodyObj = JSON.parse(body);
+      const name = bodyObj.first_name;
+      greeting = "Hi " + name + ". ";
+    }
+    //Greeting message
+    const message = greeting + "Would you like to join our Language Club?";
+    const greetingPayload = {
+      "text": message,
+      "quick_replies":[
+        {
+          "content_type":"text",
+          "title":"Yes!"//,
+          //"payload": START_REGISTRATION_YES
+        },
+        {
+          "content_type":"text",
+          "title":"No, thanks."//,
+          //"payload": START_REGISTRATION_NO
+        }
+      ]
+    };
+    callSendAPI(sender_psid, greetingPayload);
+  });
+}
+
+
+/*
+function updateStatus(sender_psid, status, callback){
+  const query = {user_id: sender_psid};
+  const update = {status: status};
+  const options = {upsert: status === GREETING};
+
+  ChatStatus.findOneAndUpdate(query, update, options).exec((err, cs) => {
+    console.log('update status to db: ', cs);
+    callback(sender_psid);
+  });
+}*/
+
+
+function handlePostback(sender_psid, received_postback) {
+  // Get the payload for the postback
+  const payload = received_postback.payload;
+
+  switch (payload){
+  	case GREETING:
+  		handleGreetingPostback(sender_psid);
+      	//updateStatus(sender_psid, payload, handleGreetingPostback);
+      	break;
+    default:
+      	console.log('Cannot differentiate the payload type');
+  }
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  console.log('message to be sent: ', response);
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "url": `${FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    console.log("Message Sent Response body:", body);
+    if (err) {
+      console.error("Unable to send message:", err);
+    }
+  });
+}
