c5b9bcf56d9f433fef7db22551a4d50bbf6735c3
Artur Kasymov <art.kasymov@gmail.com>
2019-05-27 11:27:17 +0200

    registation #1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
 const GREETING = 'GREETING';
 const START_REGISTRATION_YES = 'START_REGISTRATION_YES';
 const START_REGISTRATION_NO = 'START_REGISTRATION_NO';
+const CANCEL = 'CANCEL';
 
 //temp
 const RESEND = 'RESEND';
@@ -84,7 +85,7 @@ app.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, message) {
   	// check if it is a location message
-  	console.log('handleMEssage message:', JSON.stringify(message));
+  	console.log('handleMessage message:', JSON.stringify(message));
 
   
     //if(Math.random() >= 0.5){
@@ -98,6 +99,45 @@ function handleMessage(sender_psid, message) {
 		callSendAPI(sender_psid, resendPayload);
 		return;
 	}*/
+}
+
+function handleRegistrationStart(sender_psid) {
+  request({
+    url: `${FACEBOOK_GRAPH_API_BASE_URL}${sender_psid}`,
+    qs: {
+      access_token: process.env.PAGE_ACCESS_TOKEN,
+      fields: "first_name"
+    },
+    method: "GET"
+  }, function(error, response, body) {
+    var nicknameRequest = "";
+    if (error) {
+      console.log("Error getting user's name: " +  error);
+    } else {
+      var bodyObj = JSON.parse(body);
+      const name = bodyObj.first_name;
+      nicknameRequest = name + ", type in your nickname: ";
+    }
+    //Greeting message
+    const message = nicknameRequest;
+    const nickNamePayload = {
+      "attachment":{
+        "type": "template",
+        "payload": {
+            "template_type": "button",
+            "text": message,
+            "buttons":[
+              {
+                "type": "postback",
+                "title": "Cancel",
+                "payload": CANCEL,
+              },
+            ]
+          }      
+      }
+    };
+    callSendAPI(sender_psid, nickNamePayload);
+  });
 }
 
 function handleGreetingPostback(sender_psid){
@@ -167,6 +207,9 @@ function handlePostback(sender_psid, received_postback) {
   		handleGreetingPostback(sender_psid);
       	//updateStatus(sender_psid, payload, handleGreetingPostback);
       	break;
+    case START_REGISTRATION_YES:
+        handleRegistrationStart(sender_psid);
+        break;
     default:
       	console.log('Cannot differentiate the payload type');
   }
