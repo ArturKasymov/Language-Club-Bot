78150965f0a9b2cc922ea2993a2092f727afd5bc
Demian <dembanakh01@gmail.com>
2019-05-31 11:14:46 +0200

    fix #4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
 
-var handlers = require('./models/handlers.js');
+var handlers = require('./model/handlers.js');
 
 const
   request = require('request'),
