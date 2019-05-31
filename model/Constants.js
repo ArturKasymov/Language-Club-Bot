71e683107e6abbd3eb1a7b721ffcecf4b9803cc1
Demian <dembanakh01@gmail.com>
2019-05-31 16:16:52 +0200

    decisionTree #9

--- model/Constants.js
+++ model/Constants.js
@@ -13,7 +13,7 @@ const CANCEL = 'CANCEL';
 const got_started = "got_started";
 
 // POSTGRESQL QUERIES
-const INSERT_USER = 'INSERT INTO users VALUES($1, ' + got_started + ', NULL, NULL)';
+const INSERT_USER = 'INSERT INTO users VALUES($1, "' + got_started + '", NULL, NULL)';
 console.log("INSERT_USER: " + INSERT_USER);
 module.exports = {
     request: request,
