a983fbfe17276cb60aaeda3920dcf8980b4f21a0
Demian <dembanakh01@gmail.com>
2019-05-31 11:05:14 +0200

    fix #2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import { handleMessage, handlePostback } from './model/handlers';
-
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
 
