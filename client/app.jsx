8f2971f916d1624f8872503b7e37e54d4c1d3d73
Demian <dembanakh01@gmail.com>
2019-06-02 16:59:52 +0200

    restruct and react start #5

---
+++ client/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'whatwg-fetch';
+
+import {
+  Button,
+  ButtonArea,
+  CellBody,
+  CellFooter,
+  CellHeader,
+  CellsTitle,
+  Form,
+  FormCell,
+  Slider,
+  Switch,
+} from 'react-weui';
+
+export default class App extends React.PureComponent {
+	static dateConfig = {
+		month: 'long',
+		day: 'numeric',
+	}
+}
