d5a0403e8c1e3d60b49f84d6167a73a91b0a18ea
Demian <dembanakh01@gmail.com>
2019-06-02 21:16:22 +0200

    react #2

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './app.jsx';
+import Oops from './oops.jsx';
+
 window.attachApp = (userId) => {
    if (userId) {
         const app = <App userId={userId} />;
---
+++ client/oops.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+
+const Oops = () => {
+	return (
+		<div className='static-page'>
+			<div className='static-page-body'>
+				 <h1>Please Try in Messenger App</h1>
+
+				<div className='static-page-subtitle'>
+				  <p>This experience has been designed to work within the Messenger app on iOS or Android. Please try again there (rather than in a browser).</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Oops;
