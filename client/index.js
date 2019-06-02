846ce32494f25f907386d23e9f1712b71f41957f
Demian <dembanakh01@gmail.com>
2019-06-02 23:15:00 +0200

    react #16

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import App from './app.jsx';
 import Oops from './oops.jsx';
 
 import 'weui';
-import 'react-weui/lib/react-weui.min.css';
+import 'react-weui-css';
 
 window.attachApp = (userId) => {
    if (userId) {
