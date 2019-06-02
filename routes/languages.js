d3a07f1c868f0fa397b343cba3e60c7ddc0e1e25
Demian <dembanakh01@gmail.com>
2019-06-03 00:35:23 +0200

    react #24

--- routes/languages.js
+++ routes/languages.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('./index', {title: 'Choose Languages'});
+    res.render('./index', {title: 'Choose '});
 })
 
 export default router;
