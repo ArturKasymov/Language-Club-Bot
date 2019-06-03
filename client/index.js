45b56b8b78ea6bbf4b1b674d1badaa11d06b8ff1
Demian <dembanakh01@gmail.com>
2019-06-03 11:56:49 +0200

    react #58

--- client/index.js
+++ client/index.js
@@ -3,19 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app.jsx';
-import Oops from './oops.jsx';
+//import App from './app.jsx';
+//import Oops from './oops.jsx';
 
-import 'weui';
+//import 'weui';
 //import 'react-weui/lib/react-weui.min.css';
 
 window.attachApp = (userId) => {
     if (userId) {
-        //document.getElementById('content').getElementsByTagName('h1')[0].innerHTML = "HERR";
-        const app = <App userId={userId} />;
-        ReactDOM.render(app, document.getElementById('content'));
+        document.getElementById('content').getElementsByTagName('h1')[0].innerHTML = "userId-HERR";
+        //const app = <App userId={userId} />;
+        //ReactDOM.render(app, document.getElementById('content'));
     } else {
-        //document.getElementById('content').getElementsByTagName('h1')[0].innerHTML = "HERR";
-        ReactDOM.render(<Oops />, document.getElementById('content'));
+        document.getElementById('content').getElementsByTagName('h1')[0].innerHTML = "nouserId-HERR";
+        //ReactDOM.render(<Oops />, document.getElementById('content'));
     }
 };
