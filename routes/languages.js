f251d4015739decc98cb2c432d984d3512236b83
Demian <dembanakh01@gmail.com>
2019-06-03 07:37:28 +0200

    Update languages.js

--- routes/languages.js
+++ routes/languages.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import load from '../client/index.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('./index', {title: 'Choose Languages'});
+    res.render('./index', {title: 'Choose Languages', template: load});
 })
 
 export default router;
