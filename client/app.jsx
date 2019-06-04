266374f4f9c6c1bf7322f4b31d1b443b732367c1
Demian <dembanakh01@gmail.com>
2019-06-04 16:38:32 +0200

    react #124

--- client/app.jsx
+++ client/app.jsx
@@ -105,9 +105,9 @@ export default class App extends React.PureComponent {
 
 	addLanguage(self, lang) {
 		const oldLanguages = self.state.languages;
-		const languages = new Set(oldLanguages);
-		//languages.add(lang);
-		self.setState({languages: oldLanguages, text: lang});
+		const languages = new Array(...oldLanguages);
+		languages.push(lang);
+		self.setState({languages: languages, text: lang});
 	}
 
 	removeLanguage(self, lang) {
