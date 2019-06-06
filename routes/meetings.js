9b5d714667e09e6026017b2a3a6a2067e9a3c255
Demian <dembanakh01@gmail.com>
2019-06-06 19:19:38 +0200

    react #199

--- routes/meetings.js
+++ routes/meetings.js
@@ -41,7 +41,7 @@ router.put('/:userID', ({body, params: {userID}}, res) => {
 });
 
 router.put('/:userID/update', ({body, params: {userID}}, res) => {
-    console.log("IN PUT /MEETINGS/UPDATE " + (typeof body.startTime) + " " + body.startTime + " " + new Date(body.startTime));
+    console.log("IN PUT /MEETINGS/UPDATE " + (typeof body["startTime"]) + " " + body["startTime"] + " " + new Date(body["startTime"]) + " " + Object.keys(body));
     query(CONSTANTS.UPDATE_MEETING, [body.description, new Date(body.startTime), new Date(body.endTime), parseInt(body.id)]);
     res.sendStatus(204);
 });
