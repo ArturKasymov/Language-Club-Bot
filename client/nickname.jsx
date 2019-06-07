26d247e521d23c91236771ebea35dc78712b59c0
Artur <44034418+ArturKasymov@users.noreply.github.com>
2019-06-07 18:59:07 +0200

    Update nickname.jsx

--- client/nickname.jsx
+++ client/nickname.jsx
@@ -18,8 +18,6 @@ import {
 import WebviewControls from '../api/webview-controls';
 import Loading from './loading.jsx';
 
-import Loading from './loading.jsx';
-
 export default class Nickname extends React.PureComponent {
 
 	static propTypes = {
