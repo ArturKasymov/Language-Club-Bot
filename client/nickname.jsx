03cbcba4c70b517d429d15e0863d2db81fb8afd3
Demian <dembanakh01@gmail.com>
2019-06-07 18:33:34 +0200

    react #272

--- client/nickname.jsx
+++ client/nickname.jsx
@@ -17,6 +17,8 @@ import {
 
 import WebviewControls from '../api/webview-controls';
 
+import Loading from './loading.jsx';
+
 export default class Nickname extends React.PureComponent {
 
 	static propTypes = {
